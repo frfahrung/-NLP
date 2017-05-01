2bc269b05656acad4f20b309de7ca6d1de5fc9b4
frfahrung <frfahrung95@gmail.com>
2017-05-01 15:35:01 +0700

    rename Dict

--- Thai_NER/rule.py
+++ Thai_NER/rule.py
@@ -1,3 +1,6 @@
 
-dictList = glob.glob(os.path.join('./testing_data', '*.txt'))
-dic = [line.rstrip() for line in open('vocabulary.txt')]
+prefixPerson = [line.rstrip() for line in open('./selectClue/PerClue/person_front.txt')]
+suffixPerson = [line.rstrip() for line in open('./selectClue/PerClue/person_back.txt')]
+
+prefixOrg = [line.rstrip() for line in open('./selectClue/OrgClue/org_front.txt')]
+suffixOrg = [line.rstrip() for line in open('./selectClue/OrgClue/org_back.txt')]
